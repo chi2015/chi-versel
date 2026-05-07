20dee98493f96340c15eaeb83a45c5ec84122c17
chi2015 <sergeychi2017@gmail.com>
2026-05-07 13:15:55 +0200

    Add albums page

---
+++ albums/precache-manifest.f707dac49d65c9d3ae1bf782dd0862ec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e68e48aa7918ccb438c05352efd712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4828a361dbbb37e2136",
+    "url": "/static/js/2.cf2b21bf.chunk.js"
+  },
+  {
+    "revision": "a379e6ed96532e90f7ed",
+    "url": "/static/js/main.9cfda87a.chunk.js"
+  },
+  {
+    "revision": "d2b34cad72eb8e7a7670",
+    "url": "/static/js/runtime-main.275e8c14.js"
+  }
+]);
