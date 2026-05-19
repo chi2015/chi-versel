878956dea9201e8550417ede7c773d8ba2a6f853
chi2015 <sergeychi2017@gmail.com>
2026-05-19 17:01:17 +0200

    Update albums app

---
+++ albums/precache-manifest.5459a57762cc61facac8cc824e64b4c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b474453e72e0314df554c45dcaf46ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef22e3e575fce15881c",
+    "url": "/static/js/2.04c7d387.chunk.js"
+  },
+  {
+    "revision": "791df145003fd1912fa6",
+    "url": "/static/js/main.51dbe19e.chunk.js"
+  },
+  {
+    "revision": "d2b34cad72eb8e7a7670",
+    "url": "/static/js/runtime-main.275e8c14.js"
+  }
+]);
