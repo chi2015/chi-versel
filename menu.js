654843b88faf4596b64e024f99d7adb10f7a427f
chi2015 <sergeychi2017@gmail.com>
2026-05-21 14:48:02 +0200

    Update main menu

--- menu.js
+++ menu.js
@@ -1,50 +1,38 @@
 module.exports = [
 	{
+		title: 'Poker Chips Counter',
+		url: '/poker-chips-counter',
+		className: 'poker-chips-counter'
+	},
+
+	{
 		title: 'Albums Catalog',
 		url: '/albums',
 		className: 'albums'
 	},
-	
+
 	{
 		title: 'Playlist',
 		url: '/playlist',
 		className: 'playlist'
 	},
-	
+
 	{
 		title: 'iTunes Top 100',
 		url: '/itunes100',
 		className: 'itunes100'
 	},
-	
+
 	{
 		title: 'Citadels Game',
 		url: '/citadels',
 		className: 'citadels'
 	},
-	
+
 	{
 		title: 'Balls Break Bricks',
 		url: '/bbb',
 		className: 'bbb'
 	},
-	
-	{
-		title: 'Mafia players combinations',
-		url: '/m-combinations',
-		className: 'm-combinations'
-	},
-	
-	{
-		title: 'Mafia random number',
-		url: '/mafia-random-number',
-		className: 'mafia-tools'
-	},
-	
-	{
-		title: 'Mafia random card',
-		url: '/mafia-random-card',
-		className: 'mafia-tools'
-	},
-	
+
 ]
